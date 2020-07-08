dd8d088d52680596526c1fb2c161a792124f4e4e
Fernando José Pedrosa <fernandojpedrosa@gmail.com>
2020-07-08 16:25:25 -0300

    Testes com appsettings.

---
+++ src/APIExample/Controllers/AmbienteController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace APIExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AmbienteController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public AmbienteController(IConfiguration config)
+        {
+            this._config = config;
+        }
+
+        public string Get()
+        {
+            var ambiente = _config.GetValue<string>("CustomSetting");
+            return ambiente;
+        }
+    }
+}
